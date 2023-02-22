2acbcb53d5ed22d74680fda234232d80cdac2f4b
Renato Dias <renatocardosodias@icloud.com>
2023-02-21 22:41:01 -0300

    Dentro da View criamos 2 array, um de vídeo e outro com o nome dos videos, criamos um índice que começa com 0

--- AppHIITFit/AppHIITFit/Views/ExerciseView.swift
+++ AppHIITFit/AppHIITFit/Views/ExerciseView.swift
@@ -8,13 +8,20 @@
 import SwiftUI
 
 struct ExerciseView: View {
+
+    let index: Int
+
     var body: some View {
+
+        let videoNames = [ "squat", "step-up","burpee","sun-salute"]
+        let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
+
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 struct ExerciseView_Previews: PreviewProvider {
     static var previews: some View {
-        ExerciseView()
+        ExerciseView(index: 0)
     }
 }
