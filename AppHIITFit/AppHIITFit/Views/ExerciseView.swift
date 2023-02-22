e6446b7aed7abf8488dc1da9682eebf9acee8b26
Renato Dias <renatocardosodias@icloud.com>
2023-02-21 22:44:31 -0300

    usando o indice criando com o array de nomes de exercícios

--- AppHIITFit/AppHIITFit/Views/ExerciseView.swift
+++ AppHIITFit/AppHIITFit/Views/ExerciseView.swift
@@ -16,7 +16,7 @@ struct ExerciseView: View {
         let videoNames = [ "squat", "step-up","burpee","sun-salute"]
         let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
 
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(exerciseNames[index])
     }
 }
 
