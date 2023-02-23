6e898aae18659d1bdb76ba5e056f14ffc8099545
Renato Dias <renatocardosodias@icloud.com>
2023-02-22 21:26:58 -0300

    Criando Numero de Paginas, utilizando HStack para alinhar lado a lado os números de paginas e usado o SF Symbols para adicionar os numeros da pagina

--- AppHIITFit/AppHIITFit/Views/ExerciseView.swift
+++ AppHIITFit/AppHIITFit/Views/ExerciseView.swift
@@ -16,9 +16,26 @@ struct ExerciseView: View {
         let videoNames = [ "squat", "step-up","burpee","sun-salute"]
         let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
 
-        Text(exerciseNames[index])
-    }
-}
+        VStack {
+            VStack {
+                Text(exerciseNames[index])
+                    .font(.largeTitle)
+                HStack {
+                    Image(systemName: "1.circle")
+                    Image(systemName: "2.circle")
+                    Image(systemName: "3.circle")
+                    Image(systemName: "4.circle")
+                } //end HStack
+                .font(.title2)
+            } //end VStack
+            Text("Video player")
+            Text("Timer")
+            Text("Start/Done button")
+            Text("Rating")
+            Text("History button")
+        } //end VStack
+    } //end var body
+} //end struct ExerciseView
 
 struct ExerciseView_Previews: PreviewProvider {
     static var previews: some View {
